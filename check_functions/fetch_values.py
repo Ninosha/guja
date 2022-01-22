2a7a5f8b41a23781df989fa2479a46c7b40abb5c
ninosha <nino.maridashvili.1@iliauni.edu.ge>
2022-01-22 12:34:29 +0400

    updated fetch values function

--- check_functions/fetch_values.py
+++ check_functions/fetch_values.py
@@ -4,21 +4,25 @@ import json
 
 def fetch_values():
     """
-    :return: filename and extension if exists
+    :return: filename and extension if exists, else => False
     """
+
     file = json_check()
-    f = open(file, "r")
-    json_data = f.read()
-    if json_data:
-        final_data = json.loads(json_data)
-        filename = final_data.get("file")
-        extension = final_data.get("extension")
-        if filename and extension:
-            return filename, extension
+
+    if file:
+        f = open(file, "r")
+
+        json_data = f.read()
+
+        if json_data:
+            final_data = json.loads(json_data)
+            filename = final_data.get("file")
+            extension = final_data.get("extension")
+            if filename and extension:
+                return filename, extension
+            else:
+                return False
         else:
             return False
     else:
         return False
-
-
-print(fetch_values())
